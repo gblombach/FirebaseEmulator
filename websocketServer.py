ba6a46d9c077ad6f0ea3ee7abcfbb9bcd7b914e6
George Blombach <george.blombach@pacificomputing.com>
2023-04-12 15:17:18 -0700

    created websocket server and client html

--- websocketServer.py
+++ websocketServer.py
@@ -9,3 +9,21 @@
     File:   websocketServer.py
 """
 
+import asyncio
+import datetime
+import random
+import websockets
+
+
+async def time(websocket, path):
+    print("new connection path " + path)
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        print(now)
+        await websocket.send(now)
+        await asyncio.sleep(10)
+
+start_server = websockets.serve(time, '127.0.0.1', 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
